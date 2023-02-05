f42c8fe602ac6bcb382b8f7e4f4ee8388da41c87
Debora Choi <54413436+deborachoi3@users.noreply.github.com>
2023-02-04 21:14:03 -0800

    pog

---
+++ src/pages/lovestop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../../static/lovestop.css"
+
+const Lovestop = () => {
+    return (
+        <header>
+            <div className="background"></div>
+        </header>
+    )
+}
+
+export default Lovestop
